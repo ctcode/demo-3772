7140681f2caba620907ba15357b28d0ac264c682
ct <ct6787@gmail.com>
2015-09-25 12:35:44 +0100

    dev

--- demo-3772.js
+++ demo-3772.js
@@ -3,8 +3,7 @@ gadgets.util.registerOnLoadHandler(Init);
 
 function Init()
 {
-	var btn = document.getElementById("btnRetrieve");
-	btn.textContent += "March 2016";
+	document.getElementById("date_text").textContent = "March 2016";
 }
 
 function onclick_RetrieveEvents()
@@ -19,4 +18,28 @@ function receive_events(data)
 // callback when event data received
 {
 	console.log(data);
+
+	var event_list = "";
+	var event_count = 0;
+
+	for (var j in data)
+	{
+		var cal_data = data[j];
+
+		if (cal_data.error)
+		{
+			alert("error retrieving event data : " + cal_data.error);
+			return;
+		}
+		
+		for (var k in cal_data.events)
+		{
+			var event = cal_data.events[k];
+			
+			event_list += (event.title + '\n');
+			event_count++;
+		}
+	}
+	
+	alert("Events:\n\n" + event_list + "\n" + "Total: " + event_count);
 }
